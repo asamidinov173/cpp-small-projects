a4fca1accc122b7ac76c4eecd970415aec405320
Alikhan Samidinov <assamidinov2004@gmail.com>
2026-02-17 15:35:17 -0500

    Add library practice

---
+++ dayofweek/cmath.cpp
@@ -0,0 +1,18 @@
+// cmath.cpp
+// Alikhan Samidinov
+// Better understanding library <cmath>
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	cout << sqrt(36) << endl; // square of 6 = 36
+	cout << pow(2,5) << endl; // 2^5 = 32
+	cout << ceil(2.3) << endl; // ceiling => round up to 3
+	cout << abs(-5) << endl; // absolute value abs(-5) = 5
+	cout << floor(2.9) << endl; // floor => round down to 2
+	cout << round(2.5) << endl; // round to the nearest => 3
+	
+	return 0;
+}

ec82f51881cad2b6f683d665835eecfe9205f735
Alikhan Samidinov <assamidinov2004@gmail.com>
2026-03-05 14:01:45 -0500

    Add debugging practice file

---
+++ dayofweek/debug.cpp
@@ -0,0 +1,22 @@
+// debug.cpp
+// Alikhan Samidinov
+// This program demonstrates random numbers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	double num1, num2, num3, avg;
+	
+	cout << "Enter the first number: ";
+	cin >> num1;
+	cout << "Enter the second number: ";
+	cin >> num2;
+	cout << "Enter the third number: ";
+	cin >> num3;
+	avg = (num1 + num2 + num3) / 3;
+	cout << "The average is " << avg << endl;
+	
+	return 0;
+}
+	
